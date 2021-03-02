fd0c875c290cb28ae922429ee1b56693c8af6d27
Yanilda <synac1@users.noreply.github.com>
2021-03-02 15:15:13 -0500

    Exercise for this week 5

---
+++ week5/src/Exercise5.java
@@ -0,0 +1,7 @@
+/*
+ * Write a login program, that keeps asking the user user for username and password, and after certain amount of
+ * failed attempts the account gets locked out  
+ */
+public class Exercise5 {
+
+}
---
+++ week5/src/PracticeForLoop.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PracticeForLoop {
+	public static void main(String[] args) {
+
+		int n = 5;
+		
+		/*
+		 * double sum = 0.0;
+		 * 
+		 * for(int i = 1 ; i <= n ; i++ ) { //infinite loop sum += i; }
+		 * 
+		 * System.out.println("Sum is "+ sum); System.out.println("Average "+ (sum/n));
+		 * 
+		 * for(int i = 100 ; i <= 600 ; i += 50 ) { //infinite loop
+		 * System.out.println(i); /// printing i }
+		 */
+		
+		String pasword = "1234";
+		Scanner sc = new Scanner(System.in);
+		int i;
+		for( i = 0; i < n; i++) {
+			
+			System.out.println("Enter password: ");
+			String userInput = sc.next();
+			
+			if(userInput.equals(pasword)) {
+				System.out.println("Welcome!");
+				break;
+			}else {
+				System.out.println("Wrong password");
+			}
+			
+		}
+		if(i == 5) {
+			System.out.println("Your account has been locked out");
+		}
+
+
+	}
+}
