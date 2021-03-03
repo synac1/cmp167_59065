f6df374d779ab8fb1d13b9b53e3598f2b0929a60
Yanilda <synac1@users.noreply.github.com>
2021-03-03 09:18:41 -0500

    Add files via upload

--- week1/Exercise.java
+++ week1/Exercise.java
@@ -1,6 +1,26 @@
+
 //1. Write a program that ask the user for their name and prints a welcome message
 //also ask the user for their age and subtract their age from the current year
 //print the result as the birth year 
-public class Exercise {
+import java.util.Scanner;
 
+public class Exercise {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//input
+		System.out.println("Hello, what is your name?");
+		String name = sc.nextLine();//Waits for the user to enter their name
+		
+		System.out.println("Hello, "+name+", How old are you?");
+		int age = sc.nextInt(); // waits for the user to enter age
+		
+		//process
+		int birthYear = 2021 - age;
+		
+		//output
+		System.out.println("Your Birth Year " + birthYear );
+
+		
+	}
 }
